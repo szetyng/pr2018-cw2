3533cd3f8dedc85d77df552e4f3b71239cc7afea
Sze Tyng Lee <lee.szetyng@gmail.com>
2018-12-12 15:40:18 +0000

    commented code

--- baseline_knn.py
+++ baseline_knn.py
@@ -6,12 +6,13 @@ from scipy.spatial import distance
 import numpy as np
 import time
 
-# import json
-# with open('PR_data/feature_data.json', 'r') as f:
-#     features = json.load(f)
+import json
+with open('PR_data/feature_data.json', 'r') as f:
+    features = json.load(f)
+features = np.array(features)
 
 data = loadmat('PR_data/cuhk03_new_protocol_config_labeled.mat')
-features = loadmat('PR_data/features.mat')['features']
+#features = loadmat('PR_data/features.mat')['features']
 
 camId = data['camId'].flatten()
 filelist = data['filelist'].flatten() # list of arrays with only one value in it
@@ -29,15 +30,12 @@ for i in query_idx:
     query_labels.append(labels[i])
 query_labels = np.array(query_labels)
 
-# SETTING K HERE
-klist = [1,5,10,15,20]
-
-# Returns the k nearest neighbours' indices and distances, for each query image
+# Returns the k nearest neighbours' indices for each query image
 def knn_camspecific(k,features, labels, query_idx, gallery_idx, camId):
     knn_id = []
-    knn_dist = []
+    #knn_dist = []
 
-    for aa,query_id in enumerate(query_idx):
+    for query_id in query_idx:
         label = labels[query_id]
         cam = camId[query_id]
         query = features[query_id]
@@ -47,21 +45,19 @@ def knn_camspecific(k,features, labels, query_idx, gallery_idx, camId):
         neighbourid_dist_pair.sort(key = lambda x:x[1])
 
         kneighbour_id = [a[0] for a in neighbourid_dist_pair[:k]]
-        kdist = [a[1] for a in neighbourid_dist_pair[:k]]
+        #kdist = [a[1] for a in neighbourid_dist_pair[:k]]
 
         knn_id.append(kneighbour_id)
-        knn_dist.append(kdist)
-        print(aa)
+        #knn_dist.append(kdist)
     knn_id = np.array(knn_id)
-    knn_dist = np.array(knn_dist)
-    return knn_id, knn_dist
+    #knn_dist = np.array(knn_dist)
+    return knn_id #, knn_dist
 
 # Get k nearest neighbours
-kay = max(klist)
 start1 = time.time()
-nn_idx, nn_distances = knn_camspecific(kay,features, labels, query_idx, gallery_idx, camId)
+nn_idx = knn_camspecific(20,features, labels, query_idx, gallery_idx, camId)
 end1 = time.time()
-print(end1 - start1, 's')
+print('KNN took', end1 - start1, 's')
 
 nn_labels = np.empty(nn_idx.shape, dtype=np.uint16)
 for i,neighbours in enumerate(nn_idx):
@@ -69,6 +65,7 @@ for i,neighbours in enumerate(nn_idx):
         nn_labels[i][j] = labels[idx]
 
 # print accuracy for each k
+klist = [1,5,10,15,20]
 for k in klist:
     corr = []
     for i,neighbours in enumerate(nn_labels[:,:k]):
@@ -79,12 +76,5 @@ for k in klist:
     print(acc*100)
 
 
-# for k = 10
-corr10 = []
-for i,neighbours in enumerate(nn_distances[:,:10]):
-    if query_labels[i] in neighbours:
-        corr10.append(query_labels[i])
-acc10 = len(corr10)/len(query_labels)
-print('acc10 =', acc10)
 
 
